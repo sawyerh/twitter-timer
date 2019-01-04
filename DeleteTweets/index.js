b83d5af20266ff3a4af96325a529d6f23f1dd538
Sawyer Hollenshead <git@sawyerh.com>
2019-01-03 20:54:41 -0500

    Hi mom

---
+++ DeleteTweets/index.js
@@ -0,0 +1,147 @@
+const Twitter = require("twitter");
+const moment = require("moment");
+const tweetsToSaveForever = require("./tweetsToSaveForever");
+let log;
+
+/**
+ * Configure Twitter client
+ */
+const client = new Twitter({
+  consumer_key: process.env["CONSUMER_KEY"],
+  consumer_secret: process.env["CONSUMER_SECRET"],
+  access_token_key: process.env["ACCESS_TOKEN_KEY"],
+  access_token_secret: process.env["ACCESS_TOKEN_SECRET"]
+});
+const username = process.env["TWITTER_USERNAME"];
+const maxCount = 200;
+// anything older than this is deleted
+const oldestAllowedDate = moment().subtract(14, "days");
+const tweetMomentDateFormat = "ddd MMM DD kk:mm:ss Z YYYY";
+
+/**
+ * Azure function handler
+ */
+module.exports = async function(context) {
+  log = context.log;
+  log(
+    "Deleting tweets and likes that occurred before",
+    oldestAllowedDate.format()
+  );
+
+  try {
+    await deleteTweets();
+    await deleteLikes();
+  } catch (error) {
+    log.error(error);
+  }
+};
+
+/**
+ * Get all of {username}'s tweets and retweets
+ * @param {Number|String} maxId - Returns results with an ID less than (that is, older than) or equal to the specified ID.
+ * @returns {Promise<Object[]>}
+ */
+async function getAllTweets(maxId) {
+  const params = { count: maxCount, screen_name: username, trim_user: true };
+  if (maxId) params.max_id = maxId;
+  let tweets = await client.get("statuses/user_timeline", params);
+
+  if (tweets.length) {
+    // Make another request to ensure we get every single tweet
+    const lastTweetId = tweets[tweets.length - 1].id;
+    const nextPagesTweets = await getAllTweets(lastTweetId);
+    if (nextPagesTweets.length) tweets = tweets.concat(nextPagesTweets);
+  }
+
+  return tweets;
+}
+
+/**
+ * Get the latest 200 of {username}'s likes
+ * @returns {Promise<Object[]>}
+ */
+async function getLikes() {
+  return client.get("favorites/list", {
+    count: maxCount,
+    include_entities: false
+  });
+}
+
+/**
+ * Delete all tweets not safelisted and older than the max age
+ * TODO: Create unit test for this
+ */
+async function deleteTweets() {
+  log("Deleting tweets...");
+  const tweets = await getAllTweets();
+  const deletableTweets = tweets.filter(
+    tweet =>
+      !tweetsToSaveForever.includes(tweet.id) &&
+      moment(tweet.created_at, tweetMomentDateFormat).isBefore(
+        oldestAllowedDate
+      )
+  );
+
+  const deletedTweets = await Promise.all(deletableTweets.map(deleteTweet));
+  log(
+    `Deleted ${deletedTweets.length} tweets out of ${
+      tweets.length
+    } total tweets`
+  );
+}
+
+/**
+ * Delete a tweet
+ * @param {Object} tweet
+ * @returns {Promise<String>}
+ */
+async function deleteTweet(tweet) {
+  const id = tweet.id_str;
+
+  try {
+    const res = await client.post(`statuses/destroy`, { id, trim_user: true });
+    log(`Deleted tweet (${id}) from ${res.created_at}: `, res.text);
+    return id;
+  } catch (error) {
+    log.error(error, id);
+  }
+}
+
+/**
+ * Delete all likes older than the max age
+ * TODO: Create unit test for this
+ */
+async function deleteLikes() {
+  log("Deleting likes...");
+  const tweets = await getLikes();
+  const deletableTweets = tweets.filter(tweet =>
+    moment(tweet.created_at, tweetMomentDateFormat).isBefore(oldestAllowedDate)
+  );
+
+  const deletedTweets = await Promise.all(deletableTweets.map(deleteLike));
+  log(
+    `Deleted ${deletableTweets.length} likes out of ${
+      tweets.length
+    } total likes`
+  );
+}
+
+/**
+ * Delete a tweet
+ * @param {Object} tweet
+ * @returns {Promise<String>}
+ */
+async function deleteLike(tweet) {
+  const id = tweet.id_str;
+
+  try {
+    const res = await client.post(`favorites/destroy`, {
+      id,
+      include_entities: false
+    });
+    log(`Deleted like (${id}) from ${res.created_at}: `, res.text);
+    return id;
+  } catch (error) {
+    log.error(error, id);
+  }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+## Overview
+
+An [Azure Function](https://docs.microsoft.com/en-us/azure/azure-functions/functions-overview) that fires daily and deletes your Tweets and Twitter likes that older than 14 days.
+
+This can be easily adapted to work as an AWS Lambda or Cloud Function if you're more familiar with those platforms.
+
+## Setup
+
+1. Install Azure function CLI tools
+    ```
+    brew tap azure/functions
+    brew install azure-functions-core-tools
+    ```
+1. Install dependencies
+    ```
+    npm install
+    ```
+1. Configure app settings by copying `local.settings.example.json` as `local.settings.json`.
+1. Optionally, install the Azure Functions extension for Visual Studio Code. If you use the extension for deploying, you'll need [.NET](https://dotnet.microsoft.com/download) in your path.
+
+## Local testing
+
+1. `func host start`
+1. Then make a `POST` request to `http://localhost:7071/admin/functions/DeleteTweets` with a JSON body of `{}`
+
+## Deploying
+
+You can deploy this Function to Azure [a number of ways](https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push). I use [Visual Studio Code](https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-first-function-vs-code).
+
+In Visual Studio Code, via the Azure Functions extension:
+
+1. Click the Azure icon in the sidebar, then sign in if you haven't already
+1. Click the "Deploy to Function App..." icon
+
+## Additional info
+
+- [Create a function in Azure that is triggered by a timer](https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-scheduled-function)
